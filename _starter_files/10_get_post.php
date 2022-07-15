1ccabbdb92943d8f376f923316abd63e46004863
David <leanmachine42@yahoo.com>
2022-07-14 22:57:15 -0500

    lesson 10

--- _starter_files/10_get_post.php
+++ _starter_files/10_get_post.php
@@ -1,6 +1,24 @@
 <?php
-/* --- $_GET & $_POST Superglobals -- */
 
-/*
-  We can pass data through urls and forms using the $_GET and $_POST superglobals.
-*/
+if(isset($_POST['submit'])){
+  echo $_POST["username"];
+  echo $_POST["age"];
+}
+
+?>
+
+<!-- <a href="<?php echo $_SERVER['PHP_SELF']; ?>?username=Jack">Click</a> -->
+<!-- <a href="<?php echo $_SERVER['PHP_SELF']; ?>?username=Kavooce">Link</a> -->
+<!-- <a href="<?php echo $_SERVER['PHP_SELF']; ?>?username=Jane&age=30">Link</a> -->
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+  <div>
+    <label for="username">Username: </label>
+    <input type="text" name="username" id="username">
+  </div>
+  <div>
+    <label for="age">Age: </label>
+    <input type="text" name="age" id="age">
+  </div>
+  <input type="submit" value="Submit" name="submit">
+</form>
