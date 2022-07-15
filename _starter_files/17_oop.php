df4e9317f394eaf756d492ea469c5c0e40707da5
David <leanmachine42@yahoo.com>
2022-07-15 01:07:07 -0500

    lesson 17

--- _starter_files/17_oop.php
+++ _starter_files/17_oop.php
@@ -1,6 +1,56 @@
 <?php 
-/* --- Object Oriented Programming -- */
 
-/*
-  From PHP5 onwards you can write PHP in either a procedural or object oriented way. OOP consists of classes that can hold "properties" and "methods". Objects can be created from classes.
-*/
+
+class User {
+  public $name;
+  public $email;
+  public $password;
+
+  public function __construct($name,$email,$password){
+    $this->name = $name;
+    $this->email = $email;
+    $this->password = $password;
+  }
+
+  function set_name($name){
+    $this->name = $name;
+  }
+
+  function get_name(){
+    return $this->name;
+  }
+
+}
+
+// $user1 = new User();
+// $user2 = new User();
+// $user1->name = "kavooce";
+// $user1->email = "user@example.com";
+// $user1->password = "123";
+// $user1->set_name("jack");
+// $user2->set_name("jane");
+
+// echo $user1->name;
+// echo $user2->name;
+
+// var_dump($user1);
+// var_dump($user2);
+
+
+$user3 = new User("kavooce", "user@example.com","123");
+// var_dump($user3);
+
+class Employee extends User {
+  public function __construct($name, $email, $password, $title){
+    parent::__construct($name,$email,$password);
+    $this->title = $title;
+  }
+
+  public function get_title(){
+    return $this->title;
+  }
+}
+
+$employee1 = new Employee($user3->name,$user3->email,$user3->password,"manager");
+// var_dump($employee1);
+echo $employee1->get_title();
